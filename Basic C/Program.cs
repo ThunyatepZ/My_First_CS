445295534f579cc12d94f34b887658b032ff1f1c
Thunyatep Anantanapong <taptitan4@gmail.com>
2021-10-20 13:21:33 +0700

    My commit

---
+++ Basic C/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Basic_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mos";
+            if(name=="Mos")
+            {
+                Console.WriteLine("Hello Mos");
+            }
+            else
+            {
+                Console.WriteLine("Who are you ?");
+            }
+            Console.ReadKey();
+            
+            /*Console.Write("In put = ");
+            //Read
+            string Resalt = Console.ReadLine();
+            Console.WriteLine("Output = {0}",Resalt);
+            
+            
+            Console.ReadKey();*/
+            
+           
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            /*            
+          
+            const int a = 10;
+            
+            Console.WriteLine(a);
+            Console.WriteLine("Hello");
+        
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();*/
+        }
+    }
+}
